813df1cfbe923aced500f0689147c92155a71b09
danedsar@gmail.com <danielsar123>
2023-04-03 23:03:31 -0700

    Created Board and Cell classes, as well as method for marking legal moves

---
+++ Board.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoardModel
+{
+    internal class Board
+    {
+
+        public int Size { get; set; }
+
+        // 2-d array of Cell objects 
+        public Cell[,] theGrid { get; set; }
+
+
+        public Board(int s)
+        {
+            // 8 x 8, standard chessboard size
+            Size = s;
+
+            // create new 2D array of cell objects
+            theGrid = new Cell[Size, Size];
+
+            //fill 2D array with 64 cells, each with unique x and y coordinates. 
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+        public void MarkNextLegalMoves(Cell currentCell, string chessPiece)
+        {
+            // clear all previous legal moves
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j].LegalNextMove = false;
+                    theGrid[i, j].CurrentlyOccupied = false;
+
+                }
+            }
+
+            switch (chessPiece)
+            {
+                // Knight moves in L" shape pattern
+                case "Knight":
+                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    break;
+
+                case "King":
+                    break;
+
+                case "Rook":
+                    break;
+
+                case "Bishop":
+                    break;
+
+                case "Queen":
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Cell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoardModel
+{
+    internal class Cell
+    {
+        // represents position of cell on chessboard, ranges from #1-8
+        public int RowNumber { get; set; }
+        public int ColumnNumber { get; set; }
+
+        // true if cell has chess piece on it, false if empty
+        public bool CurrentlyOccupied { get; set; }
+
+        // determines if chess piece can legally move to cell 
+        public bool LegalNextMove { get; set; }
+
+        // constructor takes in two integers for row and column 
+        public Cell(int x, int y)
+        {
+            RowNumber = x;
+            ColumnNumber = y;
+        }
+
+    }
+}
